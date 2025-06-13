385f4bb81d5f3ab6a7bdafe2498803dc3c6fa64e
WIN11-DEV\Admin <arizomo@gmail.com>
2025-06-13 14:39:13 -0500

    Ajusta .gitignore y elimina archivos innecesarios del repo

---
+++ Calendario/.angular/cache/19.2.15/Calendario/vite/deps/primeng_api.js
@@ -0,0 +1,37 @@
+import {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+} from "./chunk-DJEG5WXB.js";
+import "./chunk-UESYRTG7.js";
+import "./chunk-4DSXYLWU.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  ConfirmEventType,
+  ConfirmationService,
+  ContextMenuService,
+  FilterMatchMode,
+  FilterOperator,
+  FilterService,
+  Footer,
+  Header,
+  MessageService,
+  OverlayService,
+  PrimeIcons,
+  PrimeTemplate,
+  SharedModule,
+  TranslationKeys,
+  TreeDragDropService
+};
---
+++ Calendario/.angular/cache/19.2.15/Calendario/vite/deps/primeng_config.js
@@ -0,0 +1,16 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-JCKJ2IUJ.js";
+import "./chunk-DJEG5WXB.js";
+import "./chunk-UESYRTG7.js";
+import "./chunk-4DSXYLWU.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
